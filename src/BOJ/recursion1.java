b79bc42c21b3d51ac9d95532a48b08bb581674b5
조윤경 <joyungyeong@0.local>
2022-06-14 19:23:23 +0900

    ing

--- src/BOJ/recursion1.java
+++ src/BOJ/recursion1.java
@@ -10,12 +10,11 @@ public class recursion1 {
         return num * factorial(num-1);
 
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
 
-
+        System.out.println(factorial(input));
 
     }
 
