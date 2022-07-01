d714f3fcf46731d32c0405ef98d30c38e5386de8
조윤경 <joyungyeong@0.local>
2022-07-01 16:53:13 +0900

    간단한 369

--- src/SWEA/P1926.java
+++ src/SWEA/P1926.java
@@ -11,15 +11,19 @@ public class P1926 {
         System.setIn(new FileInputStream("/Users/joyungyeong/Downloads/input.txt"));
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        // 10<=N<=1000
-        String bar = "-";
 
         for (int i = 1; i<=N; ++i){
-
-
-
-
-
+           String str = String.valueOf(i);
+           if (str.contains("3") || str.contains("6")||str.contains("9")){
+               for (int j=0; j<str.length();j++){
+                   if (str.charAt(j) == '3' || str.charAt(j) == '6' || str.charAt(j) == '9') {
+                       System.out.print("-");
+                   }
+               }
+               System.out.print(" ");
+           }else{
+               System.out.printf("%d " ,i);
+           }
         }
 
     }
