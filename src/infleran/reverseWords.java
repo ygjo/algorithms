af216d2bbb118adbdb744b66c2af23345439663a
qwe123 <qwe123@qwe123ui-MacBookPro.local>
2024-07-01 14:28:31 +0900

    문자 뒤집기

---
+++ src/infleran/reverseWords.java
@@ -0,0 +1,18 @@
+package infleran;
+
+import java.util.Scanner;
+
+public class reverseWords {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = in.next();
+            int j = s.length()-1;
+            for (int k = j; k >=0; k--) {
+                System.out.print(s.charAt(k));
+            }
+            System.out.println();
+        }
+    }
+}
